34a5d49d82a7a66eb488da32269611f62633fbe9
ApocalyVec <s-vector.lee@hotmail.com>
2019-06-24 21:04:38 -0400

    implement mobile hierarchy

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ var program;
 var mvMatrix, pMatrix;
 var modelView, projection;
 
+let initModelMatrix;
 
-let stack = [];
+
+let transformStack = [];
 
 function main()
 {
@@ -58,48 +60,128 @@ function main()
 
 function render()
 {
+    // meshes to render
     let redCube = cube();
     let blueCube = cube();
     let magentaCube = cube();
+    let cube4 = cube();
 
     let tetra1 = tetrahedron(1);
-    let tetra2 = tetrahedron(3);
+    let tetra2 = tetrahedron(2);
     let tetra3 = tetrahedron(5);
 
-
-    pMatrix = perspective(fovy, aspect, .1, 10);
+    // set up projection
+    pMatrix = perspective(fovy, aspect, .1, 100);
     gl.uniformMatrix4fv( projection, false, flatten(pMatrix) );
 
+    // eye coordinates
     let eye;
     let at = vec3(0.0,0.0, 0.0);
     let up = vec3(0.0, 1.0, 0.0);
-    eye = vec3(0.0, 0.0, 8.0);
+    eye = vec3(0.0, 0.0, 15.0);
     mvMatrix = lookAt(eye, at , up);
 
-    // let tetra3TransformM =translate()
+    // mesh transform matrices
+    let tetra1InitTransformM =translate(0.0, 0.0, 0.0);
+    let tetra2InitTransformM =translate(0.0, 0.0, 0.0);
+    let tetra3InitTransformM =translate(0.0, 0.0, 0.0);
+
+    let cube11InitTransformM =translate(0.0, 0.0, 0.0);
+    let cube12InitTransformM =translate(0.0, 0.0, 0.0);
+    let cube21InitTransformM =rotateY(0);
+    let cube22InitTransformM =translate(0.0, 0.0, 0.0);
+
+    // hierarchy transform matrices
+    let hie1TransM = mult(translate(0.0, 3.0, 0.0), rotateY(0));
+
+        let hie11TransM= mult(translate(-2.0, -2.0, 0.0), rotateY(0));
+
+            let hie111TransM= mult(translate(-1.0, -2.0, 0.0), rotateY(0));
+            let hie112TransM= mult(translate(1.0, -2.0, 0.0), rotateY(0));
+
+        let hie12TransM= mult(translate(2.0, -2.0, 0.0), rotateY(0));
+
+            let hie121TransM= mult(translate(-1.0, -2.0, 0.0), rotateY(0));
+            let hie122TransM= mult(translate(1.0, -2.0, 0.0), rotateY(0));
 
     //Hierarchy modeling
-    stack.push(mvMatrix); // matrix 0 saved
-        mvMatrix = mult(rotateZ(45), mvMatrix);
-        gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
-        draw(redCube, vec4(1.0, 0.0, 0.0, 1.0));
+    transformStack.push(mvMatrix); // matrix 0 saved
+        mvMatrix = mult(mvMatrix, hie1TransM);  // matrix 1  created
 
-        stack.push(mvMatrix);  // matrix 1 saved
-            mvMatrix = mult(translate(0,3.0,0.0), mvMatrix);
+        // first hierarchy
+        transformStack.push(mvMatrix); // matrix 1 saved
+            mvMatrix = mult(mvMatrix, tetra1InitTransformM);
             gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
-            draw(magentaCube, vec4(1.0, 0.0, 1.0, 1.0));
-        mvMatrix = stack.pop();  // matrix 1 popped
-    mvMatrix = stack.pop();  // matrix 0 popped
+            draw(tetra1, vec4(1.0, 1.0, 1.0, 1.0));
+        mvMatrix = transformStack.pop(); // matrix 1 retrieved
 
-    stack.push(mvMatrix);  // matrix 0 saved
-        mvMatrix = mult(translate(-1,-1,0.0), mvMatrix);
-        gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
-        draw(blueCube, vec4(0.0, 0.0, 1.0, 1.0));
-    mvMatrix = stack.pop();  // matrix 0 popped
+        transformStack.push(mvMatrix); // matrix 1 saved
+            mvMatrix = mult(mvMatrix, hie11TransM);
 
-    gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
-    draw(tetra1, vec4(1.0, 1.0, 0.0, 1.0));
+            transformStack.push(mvMatrix); //matrix 11 saved
+                mvMatrix = mult(mvMatrix, tetra2InitTransformM);
+                gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                draw(tetra2, vec4(.5, 1.0, 0.0, 1.0));
+            mvMatrix = transformStack.pop(); // matrix 11 retrieved
 
+                transformStack.push(mvMatrix); // matrix 11 saved
+                    mvMatrix = mult(mvMatrix, hie111TransM);
+
+                    transformStack.push(mvMatrix); // matrix 111 saved
+
+                        mvMatrix = mult(mvMatrix, cube11InitTransformM);
+                        gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                        draw(redCube, vec4(1.0, 0.0, 0.0, 1.0));
+                mvMatrix = transformStack.pop(); // matrix 111 retrieved
+
+            mvMatrix = transformStack.pop(); // matrix 11 retrieved
+
+                transformStack.push(mvMatrix); // matrix 11 saved
+                    mvMatrix = mult(mvMatrix, hie112TransM);
+
+                    transformStack.push(mvMatrix); // matrix 112 saved
+
+                        mvMatrix = mult(mvMatrix, cube12InitTransformM);
+                        gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                        draw(magentaCube, vec4(1.0, 0.0, 1.0, 1.0));
+                mvMatrix = transformStack.pop(); // matrix 113 retrieved
+
+            mvMatrix = transformStack.pop(); // matrix 11 retrieved
+        mvMatrix = transformStack.pop(); // matrix 1 retrieved
+
+        transformStack.push(mvMatrix); // matrix 1 saved
+            mvMatrix = mult(mvMatrix, hie12TransM);
+
+            transformStack.push(mvMatrix); //matrix 12 saved
+                mvMatrix = mult(mvMatrix, tetra3InitTransformM);
+                gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                draw(tetra3, vec4(0.3, 0.4, 1.0, 1.0));
+            mvMatrix = transformStack.pop(); // matrix 12 retrieved
+
+            transformStack.push(mvMatrix); // matrix 12 saved
+                mvMatrix = mult(mvMatrix, hie121TransM);
+
+                transformStack.push(mvMatrix); // matrix 121 saved
+
+                    mvMatrix = mult(mvMatrix, cube21InitTransformM);
+                    gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                    draw(blueCube, vec4(0.0, 0.0, 1.0, 1.0));
+                mvMatrix = transformStack.pop(); // matrix 121 retrieved
+
+            mvMatrix = transformStack.pop(); // matrix 12 retrieved
+
+            transformStack.push(mvMatrix); // matrix 12 saved
+                mvMatrix = mult(mvMatrix, hie122TransM);
+
+                transformStack.push(mvMatrix); // matrix 122 saved
+
+                    mvMatrix = mult(mvMatrix, cube22InitTransformM);
+                    gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
+                    draw(cube4, vec4(0.0, 1.0, 1.0, 1.0));
+                mvMatrix = transformStack.pop(); // matrix 122 retrieved
+
+            mvMatrix = transformStack.pop(); // matrix 12 retrieved
+        mvMatrix = transformStack.pop(); // matrix 1 retrieved
 }
 
 // mesh must be a
