61401699bb6d0135cf96aaab1256e89b4a3b4c0b
ApocalyVec <zli12@wpi.edu>
2019-06-28 14:00:38 -0400

    fix cube lighting

--- cube.js
+++ cube.js
@@ -11,30 +11,36 @@ function quad(a, b, c, d, mesh)
         vec4(  0.5, -0.5, -0.5, 1.0 )
     ];
 
-    let indices = [ a, b, c, a, c, d ];
+    let normals_vertices = [
+        vec4( -0.5, -0.5,  0.5, 0.0 ),
+        vec4( -0.5,  0.5,  0.5, 0.0 ),
+        vec4(  0.5,  0.5,  0.5, 0.0 ),
+        vec4(  0.5, -0.5,  0.5, 0.0 ),
+        vec4( -0.5, -0.5, -0.5, 0.0 ),
+        vec4( -0.5,  0.5, -0.5, 0.0 ),
+        vec4(  0.5,  0.5, -0.5, 0.0 ),
+        vec4(  0.5, -0.5, -0.5, 0.0 )
+    ];
 
+    let indices = [ a, b, c, a, c, d ];
 
     for ( let i = 0; i < indices.length; i++ )
     {
+        mesh.normals.push( normals_vertices[indices[i]][0] );
+        mesh.normals.push( normals_vertices[indices[i]][1] );
+        mesh.normals.push( normals_vertices[indices[i]][2] );
+        mesh.normals.push( normals_vertices[indices[i]][3] );
+
+
         mesh.points.push( vertices[indices[i]] );
-    }
-
-    for (let i = 0; i < mesh.points.length - 2; i = i + 3) {
-        console.log(i);
-        let a = mesh.points[i];
-        let b = mesh.points[i+1];
-        let c = mesh.points[i+2];
-        mesh.normals.push(a[0],a[1], a[2], 0.0);
-        mesh.normals.push(b[0],b[1], b[2], 0.0);
-
-        mesh.normals.push(c[0],c[1], c[2], 0.0);
-
     }
 }
 
 function cube()
 {
-    let cubeMesh = {points: [], normals: []};
+    let cubeMesh = {points: [],
+                    normals: []
+                    };
 
     quad( 1, 0, 3, 2, cubeMesh);
     quad( 2, 3, 7, 6, cubeMesh);
